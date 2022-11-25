b71e5774ce53f57eb4c68325eb67d9f8e1ec81a6
jmonk603 <91668513+jmonk603@users.noreply.github.com>
2022-11-25 11:33:55 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,22 @@
+var x,y,d,r,speed;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    x = width/2;
+    y = width/2;
+    d = 200;
+    r = d/2;
+    speed = 10;
 }
 
 function draw() {
     background(220);
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(x, y, d);
+    y+= speed;
+    if(y > height+r){
+        y = (-r)
+    }
 }
